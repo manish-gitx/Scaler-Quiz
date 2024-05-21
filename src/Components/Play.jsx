5f16a29b9e0f88df892d68ee69d5f8ce1fea099a
Manish Rachakonda <108692670+manish-gitx@users.noreply.github.com>
2024-05-21 23:39:31 +0530

    Update Play.jsx

--- src/Components/Play.jsx
+++ src/Components/Play.jsx
@@ -78,7 +78,7 @@ export default function Play() {
                                             ? isCorrect
                                                 ? 'green'
                                                 : 'red'
-                                            : 'green'
+                                            : ''
                                     }}
                                 >
                                     {option}
